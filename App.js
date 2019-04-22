05f130b109ed57e7ffdb3543efaf8916a1b25336
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-04-22 15:28:02 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+function App() {
+    return (
+    <div>
+      <input type="checkbox" />
+      <p>Cook</p>
+      <input type="checkbox" />
+      <p>Sing</p>
+      <input type="checkbox" />
+      <p>Dance</p>
+      <input type="checkbox" />
+      <p>Eat</p>
+      <input type="checkbox" />
+      <p>Pray</p>
+   </div>
+    )
+}
+
+export default App
